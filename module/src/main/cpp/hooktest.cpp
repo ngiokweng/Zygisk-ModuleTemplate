cf4590c314f0fa942fc1c3a99dcf20f3d56efcc8
NgIokWeng <2334439391@qq.com>
2024-07-05 20:53:35 +0800

    Magisk模塊開發(二)

--- module/src/main/cpp/hooktest.cpp
+++ module/src/main/cpp/hooktest.cpp
@@ -29,7 +29,7 @@ FILE *my_fopen(const char *filename, const char *modes)
 
 void hooked_callback(bytehook_stub_t task_stub, int status_code, const char *caller_path_name,
                      const char *sym_name, void *new_func, void *prev_func, void *arg){
-    LOGD("sym_name=%s, status_code=%d", sym_name, status_code);
+    LOGD("sym_name=%s, status_code=%d,  arg: %s", sym_name, status_code, reinterpret_cast<char*>(arg));
 }
 
 void my_hook()
@@ -37,11 +37,14 @@ void my_hook()
     // 因為是純native, 因此要這樣初始化
     bytehook_init(BYTEHOOK_MODE_AUTOMATIC, true);
 
+    char* callback_arg = new char[20];
+    strcpy(callback_arg, "callback arg");
+
     bytehook_hook_single(
             "liblessontest.so",
             nullptr,
             "fopen",
             reinterpret_cast<void *>(my_fopen),
             hooked_callback,
-            nullptr);
+            callback_arg);
 }
